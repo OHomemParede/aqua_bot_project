b18ca4eb146258bced36ceea2ef252b5ec7166c3
parede <dewaldman@outlook.com>
2021-03-30 03:28:47 -0300

    aqua 27/7

--- aqua_bot/uptime.py
+++ aqua_bot/uptime.py
@@ -1,21 +1,14 @@
-from os import getenv
+from flask import Flask
 from threading import Thread
-import socket
+
+app = Flask(__name__)
 
 def send_ok():
-  while True:
-    connection, _ = TCP.accept()
-    connection.send('HTTP/1.1 200 OK'.encode())
-    connection.close()
+  app.run(host='127.0.0.1', port=8080)
+  
+@app.route('/')
+def index():
+  return "OK"
 
-
-HOST = '127.0.0.1'
-PORT = getenv('PORT')
-
-TCP = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-ORIGIN = (HOST, PORT)
-
-TCP.bind(ORIGIN) 
-TCP.listen()
 
 Thread(target=send_ok, args=()).start()
